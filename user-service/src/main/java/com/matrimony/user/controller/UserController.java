05089ae734d2a2871f7fdbdf1b14a950a63bb006
DandaHarikrishna <dandahari4469@gmail.com>
2025-06-26 01:15:03 +0530

    API gateway configuration
    API gateway configuration

---
+++ user-service/src/main/java/com/matrimony/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.matrimony.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/user")
+	public String regUser() {
+		
+		return "Registration Successfull Buddy";
+	}
+
+}
